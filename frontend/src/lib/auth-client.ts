ad60fc6a7273405da06ae7311012e1a416fb34d2
Rajgupta36 <raj.gupta369@outlook.com>
2025-09-18 01:39:47 +0530

    update env

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,7 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 
+
+const baseURL = import.meta.env.VITE_BETTER_AUTH_URL || "http://localhost:8000";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:8000", // backend
+  baseURL: baseURL,
 });
 
 export const { signIn, signOut, signUp, useSession } = authClient;
